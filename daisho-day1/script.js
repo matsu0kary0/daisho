5cebc30e41f10a767592271e0beb25e4846ffff7
matsu0kary0 <dorapochi639@gmail.com>
2025-10-16 15:41:03 +0900

    20251016v07

--- daisho-day1/script.js
+++ daisho-day1/script.js
@@ -1,13 +1,13 @@
-const helloBtn = document.getElementById("helloBtn");
-const message = document.getElementById("message");
+const helloBtn =document.getElementById("helloBtn");
+const message =document.getElementById("message");
 
 helloBtn.addEventListener("click",() => {
     const hour = new Date().getHours();
-    let greeting = "こんにちは！";
+    let greeting ="こんにちは！";
     if (hour<12) {
-        greeting = "おはようございます！";
+        greeting ="おはようございます！";
     } else if (hour>=18) {
-        greeting = "こんばんは！";
+        greeting ="こんばんは！";
     }
-    message.textContent = '${greeting} GitHub Pages の公開に成功しました！';
+    message.textContent ='${greeting} GitHub Pages の公開に成功しました！';
 });
